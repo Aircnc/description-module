10a89af5252b1638655c9bbe2983d7f9fa93c10b
Blake Forrest <blakeforrest@Blakes-MacBook-Air.local>
2018-10-26 17:09:38 -0700

    added components

---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Description from './components/description.jsx';
+
+ReactDOM.render(<Description />, document.getElementById('description'));

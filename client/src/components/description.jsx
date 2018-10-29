9d0ef804daf707969bc38e3fe79f743a7dcfaec4
Blake Forrest <blakeforrest@Blakes-MacBook-Air.local>
2018-10-29 14:02:39 -0700

    outline necessary subcomponents as well as initial API call

--- client/src/components/description.jsx
+++ client/src/components/description.jsx
@@ -9,9 +9,29 @@ class Description extends React.Component {
   	}
   }
 
+  componentDidMount() {
+
+  }
+
   render() {
   	return (
-  	  <div>Hellerrrr</div>
+      <div className="description-container">
+        <div className="header-container">
+          <Header />
+        </div>
+        <div className="short-desc-container">
+          <Short />
+        </div>
+        <div className="long-desc-container">
+          <Long />
+        </div>
+        <div className="host-container">
+          <Host />
+        </div>
+        <div className="amenities-container">
+          <Amenities />
+        </div>
+      </div>
   	)
   }
 }
